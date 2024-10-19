727090c0daf76dfe3274c387ff75e946d712b86e
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-10-19 17:30:22 +0400

    html CSS JS

---
+++ script.js
@@ -0,0 +1,37 @@
+let number = 6;
+let scour = 6 * 6;
+
+if (scour > 100) {
+  console.log("მეტია ასზე");
+} else {
+  console.log("ნაკლებია ასზე");
+}
+
+let number1 = 54;
+if (number1 < 50) {
+  console.log("ნაკლებია 50 -ზე");
+} else if (number1 < 100) {
+  console.log("ნაკლებია 100-ზე მაგრამ მეტია 50 ზე ");
+} else {
+  console.log("მეტია 100-ზე");
+}
+
+let number2 = -1;
+
+if (number2 > 0) {
+  console.log("რიცხვი დადებითია");
+} else if (number2 == 0) {
+  console.log("რიცხვი 0 - ის ტოლია");
+} else {
+  console.log("რიცხვი უარყოფითია");
+}
+
+let x = 8;
+let y = 6;
+let perimeter = x * 2 + y * 2;
+
+if (perimeter < 50) {
+  console.log("ნაკლებია 50 -ზე");
+} else {
+  console.log("მეტია 50-ზე");
+}
